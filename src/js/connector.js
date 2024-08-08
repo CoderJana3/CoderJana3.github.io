9be3917d4a7d35f176ccc291495a5c90553fe86b
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-08-08 13:46:59 +0200

    changed icon url in connector.js, added test console log to estimate.js

--- src/js/connector.js
+++ src/js/connector.js
@@ -6,7 +6,8 @@ window.TrelloPowerUp.initialize({
         return [{
             //icon:'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
             //icon: 'https://github.com/CoderJana3/testcard-powerup/blob/main/src/testicon.png',
-            icon: 'https://github.com/CoderJana3/CoderJana3.github.io/main/src/testicon.png',
+            //icon: 'https://github.com/CoderJana3/CoderJana3.github.io/main/src/testicon.png', //https://github.com/CoderJana3/CoderJana3.github.io/blob/0b87e37974f133babff89a6d13ac16ecd7a84fae/src/testicon.png
+            icon: 'https://github.com/CoderJana3/CoderJana3.github.io/blob/0b87e37974f133babff89a6d13ac16ecd7a84fae/src/testicon.png',
             text: 'Estimate Size',
             callback: function(t){
                 return t.popup({
--- src/js/estimate.js
+++ src/js/estimate.js
@@ -1,3 +1,4 @@
+console.log('Hello World!');
 
 var t = TrelloPowerUp.iframe();
 
