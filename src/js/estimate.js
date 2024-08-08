2a30a72ccd9455772d41bd02a2e06a20409cfa76
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-08-08 14:32:18 +0200

    eventlistener and return of previous value in comment

--- src/js/estimate.js
+++ src/js/estimate.js
@@ -3,22 +3,22 @@ console.log('Hello World!');
 var t = TrelloPowerUp.iframe();
 
 t.render(function (){
-    return t
+  /*  return t
     .get("card", "shared", "estimate")
     .then(function(estimate){
         winwdow.estimateSize.value = estimate;
     })
-    .then(function(){
+    .then(function(){*/
         t.sizeTo("#estimate").done();
-    });
+   /* });*/
     
 });
 
-window.estimate.addEventListener("submit", function(event){
+/*window.estimate.addEventListener("submit", function(event){
     event.preventDefault(); //Stops the browser trying to submit the form itself
     return t
     .set("card", "shared", "estimate", window.estimateSize.value)
     .then(function() {
         t.closePopup();
     });
-});
+});*/
