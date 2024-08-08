70b6b2bc2f69f77c88de7ce0e2768cba5b1fcdda
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-08-08 14:28:06 +0200

    added eventlistener for submit button, added setting of previous value in estimate.js

--- src/js/estimate.js
+++ src/js/estimate.js
@@ -3,5 +3,22 @@ console.log('Hello World!');
 var t = TrelloPowerUp.iframe();
 
 t.render(function (){
-    t.sizeTo("#estimate").done();
+    return t
+    .get("card", "shared", "estimate")
+    .then(function(estimate){
+        winwdow.estimateSize.value = estimate;
+    })
+    .then(function(){
+        t.sizeTo("#estimate").done();
+    });
+    
+});
+
+window.estimate.addEventListener("submit", function(event){
+    event.preventDefault(); //Stops the browser trying to submit the form itself
+    return t
+    .set("card", "shared", "estimate", window.estimateSize.value)
+    .then(function() {
+        t.closePopup();
+    });
 });
