7743c2d17446e5c776b60d5063059e3b57671cb3
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-08-08 12:27:39 +0200

    changed path to icon in connector.js

--- src/js/connector.js
+++ src/js/connector.js
@@ -6,7 +6,7 @@ window.TrelloPowerUp.initialize({
         return [{
            //icon:'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
             //icon: 'https://github.com/CoderJana3/testcard-powerup/blob/main/src/testicon.png',
-            icon: 'https://github.com/CoderJana3/CoderJana3.github.io/blob/main/src/testicon.png',
+            icon: 'https://github.com/CoderJana3/CoderJana3.github.io/main/src/testicon.png',
             text: 'Estimate Size',
         }];
     },
