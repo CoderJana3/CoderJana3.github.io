feb3bc404a9376fd383f7978ef05a771db09b7a5
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-08-08 13:18:21 +0200

    added callback function to card-buttons in connector.js,  created estimate.html

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,10 +4,16 @@ window.TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
         //return an array at card-badges 
         return [{
-            icon:'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
+            //icon:'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
             //icon: 'https://github.com/CoderJana3/testcard-powerup/blob/main/src/testicon.png',
-            //icon: 'https://github.com/CoderJana3/CoderJana3.github.io/main/src/testicon.png',
+            icon: 'https://github.com/CoderJana3/CoderJana3.github.io/main/src/testicon.png',
             text: 'Estimate Size',
+            callback: function(t){
+                return t.popup({
+                    title: "Estimation",
+                    url: "estimate.html",
+                });
+            },
         }];
     },
 });
