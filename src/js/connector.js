da165f8bc28fa8e3ea3e94209cc1d43b50e64d66
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-09 14:39:11 +0200

    removed invalid icon links

--- src/js/connector.js
+++ src/js/connector.js
@@ -4,10 +4,8 @@ window.TrelloPowerUp.initialize({
     'card-buttons': function(t, options){
         //return an array at card-badges 
         return [{
-            //icon:'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-            //icon: 'https://github.com/CoderJana3/testcard-powerup/blob/main/src/testicon.png',
-            //icon: 'https://github.com/CoderJana3/CoderJana3.github.io/main/src/testicon.png', //https://github.com/CoderJana3/CoderJana3.github.io/blob/0b87e37974f133babff89a6d13ac16ecd7a84fae/src/testicon.png
-            icon: 'https://github.com/CoderJana3/CoderJana3.github.io/blob/0b87e37974f133babff89a6d13ac16ecd7a84fae/src/testicon.png',
+            
+            //icon: needs to be a specific format
             text: 'Estimate Size',
             callback: function(t){
                 return t.popup({
@@ -22,7 +20,7 @@ window.TrelloPowerUp.initialize({
         .then(function(estimate){
             return [
         {
-            // icon: 'https://github.com/CoderJana3/CoderJana3.github.io/blob/0b87e37974f133babff89a6d13ac16ecd7a84fae/src/testicon.png',
+            //icon: needs to be a specific format 
             text: estimate || 'No estimate',
             color: estimate ? null : 'red' //still uses color blue even though its changed in the code... why?
         },   
