759f01c831160ca030c0ad7402132de1d2958ba8
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-09 13:15:17 +0200

    added console messages to show workflow

--- src/js/estimate.js
+++ src/js/estimate.js
@@ -1,4 +1,4 @@
-console.log('Hello World!');
+
 
 var t = TrelloPowerUp.iframe();
 
@@ -7,9 +7,11 @@ t.render(function (){
     .get("card", "shared", "estimate")
     .then(function(estimate){
         window.estimateSize.value = estimate;
+        console.log('estimate set');
     })
     .then(function(){
         t.sizeTo("#estimate").done();
+        console.log('sizeTo done');
    });
     
 });
