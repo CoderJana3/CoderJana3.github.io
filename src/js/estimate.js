b79d67550911702b40c57f0d35ab37281a3eef06
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-09 13:09:10 +0200

    fixed spelling in estimate.js for estimate value, uncommented code

--- src/js/estimate.js
+++ src/js/estimate.js
@@ -3,16 +3,18 @@ console.log('Hello World!');
 var t = TrelloPowerUp.iframe();
 
 t.render(function (){
-  /*  return t
+    return t
     .get("card", "shared", "estimate")
     .then(function(estimate){
-        winwdow.estimateSize.value = estimate;
+        window.estimateSize.value = estimate;
     })
-    .then(function(){*/
+    .then(function(){
         t.sizeTo("#estimate").done();
-   /* });*/
+   });
     
 });
+
+
 
 /*window.estimate.addEventListener("submit", function(event){
     event.preventDefault(); //Stops the browser trying to submit the form itself
@@ -22,3 +24,19 @@ t.render(function (){
         t.closePopup();
     });
 });*/
+
+
+//method to check plugindata for eventlistener
+/*const fetch = require('node-fetch');
+
+fetch('https://api.trello.com/1/cards/XrvfP0qo/pluginData?key=APIKey&token=APIToken', {
+  method: 'GET'
+})
+  .then(response => {
+    console.log(
+      `Response: ${response.status} ${response.statusText}`
+    );
+    return response.text();
+  })
+  .then(text => console.log(text))
+  .catch(err => console.error(err));*/
