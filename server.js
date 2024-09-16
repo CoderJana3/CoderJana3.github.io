a42f4310d0068143bcea0c81f42eb1fdaa870faf
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-16 12:07:33 +0200

    removed parcel bundler, installed express, added functionality to server.js

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+var cors = require('cors');
+var express = require('express');
+var app = express();
+
+app.use(cors({ origin: 'https://trello.com' }));
+
+
 app.post("/auth", function(request, response){
     response.send({
         token: "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f",
