d7773d1b547cbf72168d2871914c3d746d823ec7
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-16 13:27:43 +0200

    added listener to server.js

--- server.js
+++ server.js
@@ -2,7 +2,26 @@ var cors = require('cors');
 var express = require('express');
 var app = express();
 
+//from Glitch tutorial
+app.use(compression());
+
+
 app.use(cors({ origin: 'https://trello.com' }));
+
+//From glitch tutorial
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// listen for requests :)
+// const listener = app.listen(process.env.PORT, function () {
+//   console.info(`Node Version: ${process.version}`);
+//   console.log('Trello Power-Up Server listening on port ' + listener.address().port);
+// });
+
+var server = app.listen(3000, function () {
+    console.log('Server up and running...🏃🏃🏻');
+    console.log("Listening on port %s", server.address().port);
+  });
 
 
 app.post("/auth", function(request, response){
