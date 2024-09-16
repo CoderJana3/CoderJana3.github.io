12754b29d9d68d28ac18335700691dc08493ccc6
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-16 16:34:32 +0200

    changed /auth to /auth.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static('public'));
 //     console.log("Listening on port %s", server.address().port);
 //   });
 
-app.post("/auth", function(request, response){
+app.post("/auth.js", function(request, response){
     response.send({
         token: "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f",
     });
