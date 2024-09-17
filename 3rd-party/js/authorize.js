af1245f204f97dba905fe3be53c6186dc33bbcc5
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-17 15:42:28 +0200

    changed path for fetch&post in server.js and authorize.js

--- 3rd-party/js/authorize.js
+++ 3rd-party/js/authorize.js
@@ -3,7 +3,7 @@ var password = document.getElementById("password");
 var authBtn = document.getElementById("authorize");
 authBtn.addEventListener("click", function(){
     console.log("got to 3rdParty authorize.js");
-    fetch("/src/js/auth.js", {
+    fetch("/src/js/auth", {
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({ origin: 'https://trello.com' }));
 //     console.log("Listening on port %s", server.address().port);
 //   });
 
-app.post("/src/js/auth.js", function(request, response){
+app.post("/src/js/auth", function(request, response){
     response.send({
         token: "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f",
     });
