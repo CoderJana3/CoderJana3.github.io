f0c03dcbd06c1f181f0bf7267081d89449a586a5
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-19 14:00:49 +0200

    changed order of method and headers in fetch (authorize.js)

--- 3rd-party/js/authorize.js
+++ 3rd-party/js/authorize.js
@@ -3,12 +3,13 @@ var password = document.getElementById("password");
 var authBtn = document.getElementById("authorize");
 authBtn.addEventListener("click", function(){
     console.log("got to 3rdParty authorize.js");   //try changing fetch post to fetch get and check if that method is also not allowed,
-    fetch("/auth", {                                // don't know if it would help anything but can't hurt to try
+    fetch("/auth", {  
+        method: "POST",                              // don't know if it would help anything but can't hurt to try
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json",
         },
-        method: "POST",
+        
         body:JSON.stringify({
             username: username.value,
             password: password.value,
