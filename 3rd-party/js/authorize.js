c3f3a927b8e9b36c6dfb5ce98af0e2b3d71d75e7
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-23 15:56:22 +0200

    added const response to fetch, added var test for console.log test (authorize.js)

--- 3rd-party/js/authorize.js
+++ 3rd-party/js/authorize.js
@@ -3,7 +3,7 @@ var password = document.getElementById("password");
 var authBtn = document.getElementById("authorize");
 authBtn.addEventListener("click", function(){
     console.log("got to 3rdParty authorize.js");   //try changing fetch post to fetch get and check if that method is also not allowed,
-    fetch("/auth", {  
+    const response = fetch("/auth", {  
          method: "GET",                              // don't know if it would help anything but can't hurt to try
          headers: {
         //     Accept: 'application/json',
@@ -14,19 +14,26 @@ authBtn.addEventListener("click", function(){
         //     username: username.value,
         //     password: password.value,
         // })
-    }).then(function(response){
+     });
+
+     var test = function(response){
         const token = response.token;
-        if(window.opener && typeof window.opener.authorize ==="function"){
-            window.opener.authorize(token);
-            console.log("set token to "+ token);
-        } else {
-            sessionStorage.setItem("token", token);
-            console.log("save token");
-        }
-        // setTimeout(function(){
-        //     window.close();
-        // }, 1*1000);
-    });
+        console.log("Token is set: " + token);
+     };
+     
+     //.then(function(response){
+    //     const token = response.token;
+    //     if(window.opener && typeof window.opener.authorize ==="function"){
+    //         window.opener.authorize(token);
+    //         console.log("set token to "+ token);
+    //     } else {
+    //         sessionStorage.setItem("token", token);
+    //         console.log("save token");
+    //     }
+    //     // setTimeout(function(){
+    //     //     window.close();
+    //     // }, 1*1000);
+    // });
 });
 
 //forcepush
