a59538c4761eff8f19df5444068c482a4436350f
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-23 16:04:00 +0200

    removed seperate var, added console.log to then function (authorite.js)

--- 3rd-party/js/authorize.js
+++ 3rd-party/js/authorize.js
@@ -3,37 +3,31 @@ var password = document.getElementById("password");
 var authBtn = document.getElementById("authorize");
 authBtn.addEventListener("click", function(){
     console.log("got to 3rdParty authorize.js");   //try changing fetch post to fetch get and check if that method is also not allowed,
-    const response = fetch("/auth", {  
+    fetch("/auth", {  
          method: "GET",                              // don't know if it would help anything but can't hurt to try
          headers: {
-        //     Accept: 'application/json',
-             "Content-Type": "application/json",
+            Accept: 'application/json',
+            "Content-Type": "application/json",
          },
         
         // body:JSON.stringify({
         //     username: username.value,
         //     password: password.value,
         // })
-     });
-
-     
+    }).then(function(response){
         const token = response.token;
-        console.log("Token is set: " + token);
-    
-     
-     //.then(function(response){
-    //     const token = response.token;
-    //     if(window.opener && typeof window.opener.authorize ==="function"){
-    //         window.opener.authorize(token);
-    //         console.log("set token to "+ token);
-    //     } else {
-    //         sessionStorage.setItem("token", token);
-    //         console.log("save token");
-    //     }
-    //     // setTimeout(function(){
-    //     //     window.close();
-    //     // }, 1*1000);
-    // });
+        console.log("token: " + token);
+        if(window.opener && typeof window.opener.authorize ==="function"){
+            window.opener.authorize(token);
+            console.log("set token to "+ token);
+        } else {
+            sessionStorage.setItem("token", token);
+            console.log("save token");
+        }
+        // setTimeout(function(){
+        //     window.close();
+        // }, 1*1000);
+    });
 });
 
 //forcepush
