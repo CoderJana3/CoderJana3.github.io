e6139fddad775e6b130506762ddbc4408e26b91d
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-09-23 15:52:05 +0200

    added res.send to get (index.js)

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.use(express.static('/'));
 
 app.route('/auth').all(function (req, res, next){}).get(function (req, res, next){
   console.log("Get Request Accepted");
+  res.send({
+    token: "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f",
+  })
 }).post(function (req, res, next){
   res.json({
     token: "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f",
