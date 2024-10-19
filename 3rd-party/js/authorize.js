ac1ff9f8187378dc7a672c34bd327907e1107912
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-10-19 12:23:45 +0200

    added accept in headers, changed path to /auth

--- 3rd-party/js/authorize.js
+++ 3rd-party/js/authorize.js
@@ -3,10 +3,10 @@ var password = document.getElementById("password");
 var authBtn = document.getElementById("authorize");
 authBtn.addEventListener("click", function(){
     console.log("got to 3rdParty authorize.js");   //try changing fetch post to fetch get and check if that method is also not allowed,
-    fetch("https://coderjana3.netlify.app/auth", {                                //fetch get got the status 200 so worked fine
+    fetch("/auth", {                                //fetch get got the status 200 so worked fine
          method: "POST",                              
          headers: {
-            //Accept: 'application/json',
+            Accept: 'application/json',
             "Content-Type": "application/json",
          },
         
