3dc9536bf27bb316b81a27450c19beb08e76032b
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-10-21 12:15:44 +0200

    changed server.js from express.js server to netlify serverless function

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,20 +1,32 @@
-var cors = require('cors');
-const express = require('express');
-const serverless = require('serverless-http');
-const app = express();
+const fetch = require('node-fetch');
 
-var corsOptions = {
-    origin: ['https://trello.com','https://coderjana3.github.io', 'https://coderjana3.netlify.app']
-  };
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      token: "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f"
+    })
+  }
+}
+
+
+// var cors = require('cors');
+// const express = require('express');
+// const serverless = require('serverless-http');
+// const app = express();
+
+// var corsOptions = {
+//     origin: ['https://trello.com','https://coderjana3.github.io', 'https://coderjana3.netlify.app']
+//   };
   
-  app.use(cors(corsOptions));
+//   app.use(cors(corsOptions));
 
-  //app.use(express.static('/'));
+//   //app.use(express.static('/'));
 
-  app.post('/auth', function (request, response){
-    response.send({
-        token: "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f",
-    });
-  });
+//   app.post('/auth', function (request, response){
+//     response.send({
+//         token: "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f",
+//     });
+//   });
 
-  module.exports.handler = serverless(app);
+//   module.exports.handler = serverless(app);
