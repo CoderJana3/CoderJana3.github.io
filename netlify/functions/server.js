18f87c3546e19f78218a6149e0b95e829eecd25a
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-10-21 12:35:12 +0200

    defined token outside of return (server.js)

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,11 +1,10 @@
 
 exports.handler = async function(event, context) {
   const eventBody = event.Body
+  const token = "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f"
   return {
     statusCode: 200,
-    body: {
-      token: "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f"
-    }
+    body: token
   }
 }
 
