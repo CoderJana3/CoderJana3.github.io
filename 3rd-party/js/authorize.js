1a46c0793424de3b7997f6fd66ee0a3e4a4cb357
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-11-18 15:53:50 +0100

    added body to def of token (authorize.js)

--- 3rd-party/js/authorize.js
+++ 3rd-party/js/authorize.js
@@ -15,8 +15,8 @@ authBtn.addEventListener("click", function(){
             password: password.value,
         })                              //fetch gets the repsonse from my server.js, so take-
     }).then(function(response){         // -another look at how to specify response body in functions & how to get the data from the response body
-        response => response.JSON();    //try to remove this and return in server.js just token as string
-        const token = response.token; //maybe response.token here is why token is undefined? 
+        //response => response.JSON();    //try to remove this and return in server.js just token as string
+        const token = response.body.token; //maybe response.token here is why token is undefined? 
         console.log("Response Status: " + response.status);
         console.log("Token is: " + token);
         if(window.opener && typeof window.opener.authorize ==="function"){
