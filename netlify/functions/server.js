f136ea27a3db10cbf922957b630f36a833f03dbc
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-11-18 16:00:30 +0100

    added console.log for body to check if function gets defined input (server.js)

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -4,7 +4,7 @@ const token = "198374638a1caca81e1827376460201982baed5155e6c4934784625fa52372f"
 
 exports.handler = async function(event, context) {
   const eventBody = event.Body
-
+  console.log(eventBody.username);
   return {
     statusCode: 200,
     body: JSON.stringify({
