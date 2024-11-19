65510e43d77d2705ad2f50528fc91ef92ffb27fb
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-11-19 18:19:11 +0100

    removed body again (authorize.js)

--- 3rd-party/js/authorize.js
+++ 3rd-party/js/authorize.js
@@ -16,9 +16,10 @@ authBtn.addEventListener("click", async () => {
         })                              
     }).then(//function(response){         
         response => response.json())    //JSON  is wrong cause it is not a function, the function is json()
-        const resp = JSON.stringify(response); 
-        console.log("Response Status:" + resp.body.statusCode);
-        console.log("Token is: " + resp.body.token);
+        const resp = JSON.stringify(response);
+        console.log("Response: " + resp); 
+        console.log("Response Status:" + resp.statusCode);
+        console.log("Token is: " + resp.token);
         if(window.opener && typeof window.opener.authorize ==="function"){
             window.opener.authorize(resp.token);
             console.log("set token to "+ resp.token);
