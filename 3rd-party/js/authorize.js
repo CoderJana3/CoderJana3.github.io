eabd7a51d90aa135e458195971c0e4c6ff56fb9d
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-11-19 17:46:10 +0100

    changed response back to token(Token is) and changed response to token(Response Status) (authorize.js)

--- 3rd-party/js/authorize.js
+++ 3rd-party/js/authorize.js
@@ -17,8 +17,8 @@ authBtn.addEventListener("click", async () => {
     }).then(//function(response){         
         response => response.json())    //JSON  is wrong cause it is not a function, the function is json()
         const token = JSON.stringify(response); 
-        console.log("Response Status: " + response.statusCode);
-        console.log("Token is: " + response);
+        console.log("Response Status: " + token.statusCode);
+        console.log("Token is: " + token);
         if(window.opener && typeof window.opener.authorize ==="function"){
             window.opener.authorize(token);
             console.log("set token to "+ token);
