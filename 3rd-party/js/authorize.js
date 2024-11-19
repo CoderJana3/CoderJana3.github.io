68a5df0af54db2e71daa87da7e4d4dffada3ba69
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2024-11-19 17:25:54 +0100

    added stringify to const token (authorize.js)

--- 3rd-party/js/authorize.js
+++ 3rd-party/js/authorize.js
@@ -16,7 +16,7 @@ authBtn.addEventListener("click", async () => {
         })                              //fetch gets the repsonse from my server.js, so take-
     }).then(//function(response){         // -another look at how to specify response body in functions & how to get the data from the response body
         response => response.JSON());    //try to remove this and return in server.js just token as string
-        const token = response; //maybe response.token here is why token is undefined? 
+        const token = JSON.stringify(response); //maybe response.token here is why token is undefined? 
         console.log("Response Status: " + response.status);
         console.log("Token is: " + token);
         if(window.opener && typeof window.opener.authorize ==="function"){
