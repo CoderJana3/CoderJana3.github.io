5afaeba7b6f57b69873e7d6701d6ab83a32de1cb
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-01-11 13:22:17 +0100

    changed apikey type(connector.js)

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,11 +1,11 @@
 //console.log('Hello World!');
 /*Getting and setting the API_KEY to use later on for t.getRestApi()*/
-const apikey = "";
+const apikey = null;
  async function getEnv(envkey) {
    const envvar = await fetch("/.netlify/functions/envvar")    //need this to get environemnt vars from netlify 
         .then(envvar => envvar.json());  
    envkey = '"' + envvar.apikey + '"'; 
-   return envkey;                    //save apikey in the actual thing not just null
+   return envkey;                    
 };
 apikey = getEnv(apikey);                                                                               
 
