3361983197875de8f55f61877994764c9121ef13
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-01-11 13:41:13 +0100

    changed const to var for isAuth(connector.js)

--- src/js/connector.js
+++ src/js/connector.js
@@ -131,7 +131,7 @@ window.TrelloPowerUp.initialize({
                 const testkey2 = JSON.stringify(envvar.testkey);
                 console.log("Test 1: " + testkey1, '\n', "Test 2: " + testkey2);
 
-                const isAuth = undefined;
+                var isAuth = undefined;
                 function authorizedTest (t, auth) {
                     return t
                       .getRestApi()
