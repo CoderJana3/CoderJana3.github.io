7040880504cee48b51109f8933d43d442752936e
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-01-12 15:01:55 +0100

    changed var closeBtn to authBtn(tauthf.js)

--- src/js/tauthf.js
+++ src/js/tauthf.js
@@ -4,7 +4,7 @@ t.render(function(){
     return t.sizeTo("#content");
 });
 
-var closeBtn = document.getElementById("auth");
-closeBtn.addEventListener("click", function(){
+var authBtn = document.getElementById("auth");
+authBtn.addEventListener("click", function(){
     return t.closePopup();                                  //call authorize at this point but for testing just close
 });
