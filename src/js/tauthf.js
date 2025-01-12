8d79b31b440609f15161cacccddd4761a93dd863
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-01-12 15:00:00 +0100

    changed which ID to check for Btn (tauthf.js)

--- src/js/tauthf.js
+++ src/js/tauthf.js
@@ -4,7 +4,7 @@ t.render(function(){
     return t.sizeTo("#content");
 });
 
-var closeBtn = document.getElementById("close");
+var closeBtn = document.getElementById("auth");
 closeBtn.addEventListener("click", function(){
     return t.closePopup();                                  //call authorize at this point but for testing just close
 });
