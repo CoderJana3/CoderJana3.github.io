efe9edc103f05f6023c3f0320511e283de7a2c06
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-01-14 13:35:12 +0100

    added code to get Apikey and then input it for iframe(tauthf.js&tautht.js)

--- src/js/tauthf.js
+++ src/js/tauthf.js
@@ -1,11 +1,27 @@
-var t = window.TrelloPowerUp.iframe();
+/*Getting and setting the API_KEY to use later on for t.getRestApi()*/
+var apikey = undefined;
+async function getEnv(envkey) {
+   const envvar = await fetch("/.netlify/functions/envvar")    //need this to get environemnt vars from netlify 
+        .then(envvar => envvar.json());  
+   envkey = '"' + envvar.apikey + '"'; 
+   return envkey;                    
+};
+apikey = getEnv(apikey); 
 
+/*Creating Trello iframe with Information needed for t.getRestApi()*/
+var t = window.TrelloPowerUp.iframe({
+    appKey: apikey,
+    appName: "TestCard-PowerUp",
+    appAuthor: "J D",
+});
+
+/*Render for popup size window*/
 t.render(function(){
     return t.sizeTo("#content");
 });
 
 
-
+/*Define what happens on clicking the Button in the popup*/
 var authBtn = document.getElementById("auth");
 authBtn.addEventListener("click", async function(){
     /*Section: Getting Card-ID*/
--- src/js/tautht.js
+++ src/js/tautht.js
@@ -1,10 +1,28 @@
-var t = window.TrelloPowerUp.iframe();
+/*Getting and setting the API_KEY to use later on for t.getRestApi()*/
+var apikey = undefined;
+async function getEnv(envkey) {
+   const envvar = await fetch("/.netlify/functions/envvar")    //need this to get environemnt vars from netlify 
+        .then(envvar => envvar.json());  
+   envkey = '"' + envvar.apikey + '"'; 
+   return envkey;                    
+};
+apikey = getEnv(apikey); 
 
+/*Creating Trello iframe with Information needed for t.getRestApi()*/
+var t = window.TrelloPowerUp.iframe({
+    appKey: apikey,
+    appName: "TestCard-PowerUp",
+    appAuthor: "J D",
+});
+
+/*Render for popup size window*/
 t.render(function(){
     return t.sizeTo("#content");
 });
 
+/*Define what happens on clicking the Button in the popup*/
 var closeBtn = document.getElementById("close");
 closeBtn.addEventListener("click", function(){
+    //in here should be all the code that's in tauthf.js right now for testing
     return t.closePopup();
 });
