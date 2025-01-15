733cc6d06e0af02f6f6f1279b31c9f7681840ae9
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-01-15 11:03:00 +0100

    added if keyLooksValid to EvenListener(tauthf.js)

--- src/js/tauthf.js
+++ src/js/tauthf.js
@@ -13,11 +13,9 @@ async function getEnv(envkey) {
 const testkeyNum = "g98374638a1caca81e18273764602015";
 const keyLooksValid = function(testK) {                         //from Trello Power Up Example from glitch
     // If this returns false, the Promise won't resolve.
-    //return /^[A-Za-z0-9]{32}$/.test(testK);
-    return /\w{32}/.test(testK);
+    return /^[A-Za-z0-9]{32}$/.test(testK);
   } 
-apikey = getEnv(apikey); 
-console.log(apikey);
+apikey = getEnv(apikey); //for some reason this returns a promise, fulfilled but not just the value
 const isTestkey = keyLooksValid(testkeyNum);
 const isKey = keyLooksValid(apikey);
 console.log("valid TestKey: " + isTestkey + "\n" + "validKey: " + isKey);
@@ -48,18 +46,23 @@ t.render(function(){
 /*Define what happens on clicking the Button in the popup*/
 var authBtn = document.getElementById("auth");
 authBtn.addEventListener("click", async function(){                       //try adding async/await for getRestApi -> should solve the Problem if it'S
-    console.log("Not Authorized!")                                  //waiting on object Promise it didn't
-    await t.getRestApi()
-    .authorize({scope:"read"})
-    .then(function(t){
-        console.log("Successfully authorized!");
-        t.alert("Success!")
-        return t.closePopup();
+    console.log("Not Authorized!")  
+    if(keyLooksValid(apikey)){
+        await t.getRestApi()
+               .authorize({scope:"read"})
+               .then(function(t){
+                    console.log("Successfully authorized!");
+                     t.alert("Success!")
+                    return t.closePopup();
         
-    }).catch(TrelloPowerUp.restApiError.AuthDeniedError, function () {
-        console.log("Error while authorizing: User denied Authorization");
-        alert("Cancelled!");
-      });
+                 }).catch(TrelloPowerUp.restApiError.AuthDeniedError, function () {
+                        console.log("Error while authorizing: User denied Authorization");
+                         alert("Cancelled!");
+                    });
+    } else {
+        alert("No valid APIKey!");
+    }                                //waiting on object Promise it didn't
+    
                                                     
     //return t.closePopup();                                 //call authorize at this point but for testing just close
 });
