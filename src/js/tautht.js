78469c7632ec0057d676d37baee9da8fa221d95d
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-01-20 16:23:44 +0100

    cleaned up comments (tautht.js)

--- src/js/tautht.js
+++ src/js/tautht.js
@@ -1,45 +1,45 @@
-/*Getting and setting the API_KEY to use for t.getRestApi()*/
+/**Getting and setting the API_KEY to use for t.getRestApi()*/
 async function getEnv() {
-    /*Getting the APIKey saved in netlify environment Variables */
+    /**Getting the APIKey saved in netlify environment Variables */
    const envvar = await fetch("/.netlify/functions/envvar")                     //need this to get environemnt vars from netlify 
         .then(envvar => envvar.json());  
     var envkey = envvar.apikey;                                                 //save the apikey in var envkey to use it later on
                                                                                 //as the apikey is already stringified in the Body of the response there is no need to 
                                                                                 //use stringify again
    
-   /*Creating Trello iframe with Information needed for t.getRestApi()*/
+   /**Creating Trello iframe with Information needed for t.getRestApi()*/
     var t = window.TrelloPowerUp.iframe({
         appKey: envkey,
         appName: "TestCard-PowerUp",
         appAuthor: "J D",
     });
 
-    /*Render for popup size window*/
+    /**Render for popup size window*/
     t.render(function(){
         return t.sizeTo("#content");                                            //this could be nicer, check what options there are for render
     });
 
-    /*Define what happens on clicking the Close Button in the Popup*/
+    /**Define what happens on clicking the Close Button in the Popup*/
     var closeBtn = document.getElementById("close");                            
     closeBtn.addEventListener("click", async function(){  
 
-        /*Section: Getting Card-ID*/
+        /**Section: Getting Card-ID*/
         var context = t.getContext();                                           //gives JSON-Resp with detailed information
         console.log(JSON.stringify(context, null, 2));                          //gives the correct Card Information
         console.log("Card-ID:" + context.card);                                 //Test to check that the ID was correct
 
-        /*Section:TokenTest Function Defintion*/
+        /**Section:TokenTest Function Defintion*/
         const tokenLooksValid = function(testtoken) {                           //from Trello Power Up Example from glitch
             // If this returns false, the Promise won't resolve.
             return /^[0-9a-f]{64}$/.test(testtoken);                            //does not work right now ;(
         } 
         
-        /*Section: start creating the URL */                                    //create a Base URL with the information that's already available, aka card ID and API Key
+        /**Section: start creating the URL */                                    //create a Base URL with the information that's already available, aka card ID and API Key
         var testURL = 'https://api.trello.com/1/cards/' + context.card + '/attachments?key=';
         var URL = 'https://api.trello.com/1/cards/' + context.card + '/attachments?key=' + envkey + '&token='; 
         //'https://api.trello.com/1/cards/{id}/attachments?key=APIKey&token=APIToken'   //without API Key and API Token 401 error
 
-        /*Section: Getting Token and making request*/
+        /**Section: Getting Token and making request*/
         var gottoken = undefined;                                               //Test Variable to check that there was a Token returned
         var validtoken = false;                                                 //Test Variable to check if the Token that was returned is valid
         await t.getRestApi()                                                    //keep await here, so that log tests come out in correct order
@@ -73,7 +73,7 @@ async function getEnv() {
 getEnv();                                                                       //Call getEnv() to allow all Functions to work
 
 
-/* OLD CODE*/
+/**OLD CODE*/
 // /*Creating Trello iframe with Information needed for t.getRestApi()*/
 // var t = window.TrelloPowerUp.iframe({
 //     appKey: apikey,
