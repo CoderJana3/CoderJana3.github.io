10df1bce4cf207abaf1c85b262b712766281d38f
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-01-20 13:10:08 +0100

    added test to log(tauthf.js)

--- src/js/tauthf.js
+++ src/js/tauthf.js
@@ -13,7 +13,7 @@ async function getEnv() {
 var promiseKey = "";
 var test = getEnv();
 if(test instanceof Promise){
-    console.log("Test is a Promise")
+    console.log("Test is a Promise" + test)
 } else {
     console.log("Test is not a Promise")
 }
