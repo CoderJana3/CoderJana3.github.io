2843b717309c1291a8904dd0589514ea256d9cc2
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-02-21 16:56:28 +0100

    moved fetch inside eventlistener, removed fetchJSONData call(mapping.js)

--- src/js/mapping.js
+++ src/js/mapping.js
@@ -14,7 +14,17 @@ function fetchJSONData() { fetch('./src/json/test.json')            //copied fro
 
 var mapbtn = document.getElementById("mapbtn");
 mapbtn.addEventListener("click", async function(){
-        var JSONdata = await fetchJSONData(); 
+        //var JSONdata = await fetchJSONData(); 
+        var JSONData = await fetch('./src/json/test.json')            //copied from https://www.geeksforgeeks.org/read-json-file-using-javascript/
+        .then(response => {
+             if (!response.ok) {
+             throw new Error(`HTTP error! Status: ${response.status}`);
+         }   
+             response = response.json();  
+         })
+        .then(data => console.log(data))  
+        .catch(error => console.error('Failed to fetch data:', error)); 
+
         console.log("JSONData Test, give out Data ID: " + JSONdata.id);
         //console.log(JSONtest);
 });
