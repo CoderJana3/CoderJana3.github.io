cd481c12d34554936025204db07695c482a3195b
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-02-21 17:06:06 +0100

    added JSONData assignment (mapping.js)

--- src/js/mapping.js
+++ src/js/mapping.js
@@ -16,9 +16,7 @@ var mapbtn = document.getElementById("mapbtn");
 mapbtn.addEventListener("click", async function(){
         //var JSONdata = await fetchJSONData(); 
         var JSONData = await fetch('./src/json/test.json')            //copied from https://www.geeksforgeeks.org/read-json-file-using-javascript/
-        .then(response => 
-             
-            response.json() 
+        .then(response => {JSONData = response.json() }
          )
         .then(data => console.log(data))  
         .catch(error => console.error('Failed to fetch data:', error)); 
