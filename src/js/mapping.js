352ab2fd2b011b92a79713423f07bd1a5086064d
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-02-21 16:33:38 +0100

    removed module(mapping.html), added fetchJSONData() (mapping.js)

--- src/js/mapping.js
+++ src/js/mapping.js
@@ -1,6 +1,15 @@
 
-import * as data from '/src/json/test.json';
-var JSONtest = data;
+fetchJSONData() = fetch('./sample.json')
+               .then(response => {
+                    if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }   
+                    return response.json();  
+                })
+                .then(data => console.log(data))  
+                .catch(error => console.error('Failed to fetch data:', error)); 
+
+var JSONtest = fetchJSONData(); 
 
 var mapbtn = document.getElementById("mapbtn");
 mapbtn.addEventListener("click", function(){
