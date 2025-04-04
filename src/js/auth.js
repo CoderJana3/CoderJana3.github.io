bbbd80e1a75863adb9d6fa0f77bdea6fe2c98113
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-04-04 17:35:13 +0200

    corrected oauthUrl (auth.js)

--- src/js/auth.js
+++ src/js/auth.js
@@ -6,7 +6,7 @@ t.render(function(){
 
 //var oauthUrl = window.origin + "/3rd-party/authorize.html";
 //var oauthUrl = "https://api.planbic.de/auth/login"
-var oauthUrl = window.origin + "bicauth.html";
+var oauthUrl = window.origin + "/bicauth.html";
 
 var authBtn = document.getElementById("authorize");
 authBtn.addEventListener("click", function(){
