5e614a717db40b89ccca74bbc570ff59fdec517a
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-04-04 17:44:00 +0200

    changed if to check value not element (bicauth.js)

--- src/js/bicauth.js
+++ src/js/bicauth.js
@@ -4,11 +4,11 @@ var password = document.getElementById("passw");
 var authBtn = document.getElementById("authorize");
 
 authBtn.addEventListener("click", function(){
-    if(username == "test"){
+    if(username.value == "test"){
         console.log("Username was transmitted!");
     } else { console.log("No username submitted!")}
     
-    if(password == "123"){
+    if(password.value == "123"){
         console.log("Password was transmitted")
     } else { console.log("No password submitted!")}
 })
