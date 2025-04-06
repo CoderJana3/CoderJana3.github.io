6f8018d9467e1fe7cc25829bb5f68e5bdd908634
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-04-06 15:25:19 +0200

    added comment(bicauth.js), added empty space(.gitignore)

--- src/js/bicauth.js
+++ src/js/bicauth.js
@@ -3,7 +3,9 @@ var username = document.getElementById("usenam");
 var password = document.getElementById("passw");
 var authBtn = document.getElementById("authorize");
 //var bicauthURL = "https://api.planbic.de/auth/login"
-var bicauthURL = "/login.js"
+var bicauthURL = "/login.js" //redirect is unneccessary because it still causes CORS error 
+// Quellübergreifende (Cross-Origin) Anfrage blockiert: Die Gleiche-Quelle-Regel verbietet das Lesen der externen Ressource auf 
+// (Grund: CORS-Anfrage schlug fehl). Statuscode: (null).
 
 authBtn.addEventListener("click", async() =>{
     if(username.value == "test"){
