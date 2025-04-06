84e58729a8434a31da7d015a10999863fa839a92
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-04-06 13:20:35 +0200

    changed the path from /auth/login to /login(netlify.toml)

--- src/js/bicauth.js
+++ src/js/bicauth.js
@@ -13,7 +13,7 @@ authBtn.addEventListener("click", async() =>{
         console.log("Password was transmitted")
     } else { console.log("No password submitted!")}
 
-    fetch("/auth/login", {
+    fetch("/login", {
         method: 'GET',
         headers: {
             'Accept': 'WWW-Authenticate'
