dc351a4840aafc72380f8d2a1334b40cecf8f7b2
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-04-06 13:56:00 +0200

    defined url before using (bicauth.js)

--- src/js/bicauth.js
+++ src/js/bicauth.js
@@ -3,6 +3,7 @@ var username = document.getElementById("usenam");
 var password = document.getElementById("passw");
 var authBtn = document.getElementById("authorize");
 //var bicauthURL = "https://api.planbic.de/auth/login"
+var bicauthURL = "/login.js"
 
 authBtn.addEventListener("click", async() =>{
     if(username.value == "test"){
@@ -13,7 +14,7 @@ authBtn.addEventListener("click", async() =>{
         console.log("Password was transmitted")
     } else { console.log("No password submitted!")}
 
-    fetch("/login.js", {
+    fetch(bicauthURL, {
         method: 'GET',
         headers: {
             'Accept': 'WWW-Authenticate'
