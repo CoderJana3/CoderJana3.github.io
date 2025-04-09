465b74ee41240a19467a162547e21de029c7a2c4
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-04-09 12:27:08 +0200

    changed URL(bicauth.js)

--- src/js/bicauth.js
+++ src/js/bicauth.js
@@ -2,8 +2,8 @@
 var username = document.getElementById("usenam");
 var password = document.getElementById("passw");
 var authBtn = document.getElementById("authorize");
-//var bicauthURL = "https://api.planbic.de/auth/login"
-var bicauthURL = "/login.js" //redirect is unneccessary because it still causes CORS error 
+var bicauthURL = "https://api.planbic.de/auth/login"
+var bicauthURLlog = "/login.js" //redirect is unneccessary because it still causes CORS error 
 // Quellübergreifende (Cross-Origin) Anfrage blockiert: Die Gleiche-Quelle-Regel verbietet das Lesen der externen Ressource auf 
 // (Grund: CORS-Anfrage schlug fehl). Statuscode: (null).
 var bicauthURL2 = "/auth/login"
@@ -17,14 +17,14 @@ authBtn.addEventListener("click", async() =>{
         console.log("Password was transmitted")
     } else { console.log("No password submitted!")}
 
-    // fetch(bicauthURL, {
+    // fetch(bicauthURLlog, {
     //     method: 'GET',
     //     // headers: {
     //     //     'Accept': 'WWW-Authenticate'
     //     // }
     // }).catch(err => console.error("login.js "+err));
 
-    fetch(bicauthURL2, {
+    fetch(bicauthURL, {
         method: 'POST',
         headers: {
             'Access-Control-Allow-Origin': 'www.api.planbic.de'
