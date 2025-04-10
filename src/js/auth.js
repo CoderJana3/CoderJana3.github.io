444fefac3f7e87b967fed4e3b05efa2aaf66a62a
Jana Dahlmann <jana.dahlmann@tu-dortmund.de>
2025-04-10 12:09:05 +0200

    changed URL to secURL (auth.js)

--- src/js/auth.js
+++ src/js/auth.js
@@ -7,10 +7,11 @@ t.render(function(){
 //var oauthUrl = window.origin + "/3rd-party/authorize.html";
 //var oauthUrl = "https://api.planbic.de/auth/login"
 var oauthUrl = window.origin + "/bicauth.html";
+var secURL = "https://app.planbic.de/login"
 
 var authBtn = document.getElementById("authorize");
 authBtn.addEventListener("click", function(){
-    t.authorize(oauthUrl)
+    t.authorize(secURL)
     .then(function(token){
         return t.storeSecret('token', token);
         //return t.set("member", "private", "authToken", token);
